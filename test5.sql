39cc59dc93e07ff3961566262a9034c40cd5085e
naumansqb <naumansqb@gmail.com>
2025-11-24 09:24:30 -0500

    Update test5.sql
    fixed to amtch color make and model

--- test5.sql
+++ test5.sql
@@ -1,6 +1,7 @@
--- 5. Find all Dealerships that have a certain car type (i.e. Red Ford Mustang)
 SELECT d.name, d.address, d.phone, v.*
 FROM dealerships d
 JOIN inventory i ON i.dealership_id = d.dealership_id
 JOIN vehicles v ON v.VIN = i.VIN
-WHERE v.vehicleType= "SUV";
+WHERE v.color = 'Red'
+  AND v.make = 'Nissan'
+  AND v.model = 'Altima';
